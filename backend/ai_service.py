b1efecdc56659385d04d89f34a1a78b35fba0eb9
TG-Codes <tgblinkss@gmail.com>
2025-05-31 14:36:35 +0100

    commit

--- backend/ai_service.py
+++ backend/ai_service.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 api_key = 'AIzaSyCASu0OrwpkroHYUslJuo_ZHAiQ_NHfZMs'
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 question = sys.argv[1]
 
